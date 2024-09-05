3b38dfea3a202fdbf6c266724ed5c0c54dcc0932
chere3 <71246795+Chere3@users.noreply.github.com>
2024-09-05 15:49:20 -0600

    P6 base de datos

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+    DEBUG = True
+    SECRET_KEY = 'sbxJfPvf27kmD-X2UpkipWlK7KIA'
+    
+class DevelopmentConfig:
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'thePeoplesProyect'
