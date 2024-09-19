ea0980776211df244927e15a28c4cda833613261
chere3 <71246795+Chere3@users.noreply.github.com>
2024-09-19 16:04:17 -0600

    practica 7: registro

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 class Config:
     DEBUG = True
-    SECRET_KEY = 'sbxJfPvf27kmD-X2UpkipWlK7KIA'
-    
-class DevelopmentConfig:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = 'thePeoplesProyect'
+    SECRET_KEY = "sbxJfPvf27kmD-X2UpkipWlK7KIA"
+    PORT = 3300
+
+
+class DevelopmentConfig(Config):
+    MYSQL_HOST = "localhost"
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = ""
+    MYSQL_DB = "thepeoplelproyect"
--- thePeoplesProyect.py
+++ thePeoplesProyect.py
@@ -1,26 +1,65 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
 
 thePeoplesProyect = Flask(__name__)
+db = MySQL(thePeoplesProyect)
 
-@thePeoplesProyect.route('/')
+thePeoplesProyect.config.from_object("config.DevelopmentConfig")
+
+
+@thePeoplesProyect.route("/")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
-@thePeoplesProyect.route('/auth/register')
+
+@thePeoplesProyect.route("/auth/register", methods=["GET", "POST"])
 def signup():
-    return render_template('signup.html')
+    if request.method == "POST":
+        # Get the user info from the form
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        encryptedPassword = generate_password_hash(password)
+        registeredDate = datetime.now()
 
-@thePeoplesProyect.route('/auth/login')
+        # Check if user already exists
+        cursor = db.connection.cursor()
+        cursor.execute("SELECT * FROM usuarios WHERE correo = %s", [email])
+        user = cursor.fetchone()
+        if user:
+            return "User already exists"
+
+        # Insert user info the database
+        cursor.execute(
+            "INSERT INTO usuarios (nombre, correo, clave, fecha) VALUES (%s, %s, %s, %s)",
+            (name, email, encryptedPassword, registeredDate),
+        )
+
+        # Close the cursor and commit the changes
+        db.connection.commit()
+        cursor.close()
+
+        # Send to home page
+        return render_template("home.html")
+
+    return render_template("signup.html")
+
+
+@thePeoplesProyect.route("/auth/login")
 def signin():
-    return render_template('signin.html')
+    return render_template("signin.html")
 
-@thePeoplesProyect.route('/api')
+
+@thePeoplesProyect.route("/api")
 def api():
     return {
         "name": "thePeoplesProyect",
         "version": "1.0.0",
-        "description": "A simple API for the people"
+        "description": "A simple API for the people",
     }
 
-if __name__ == '__main__':
-    thePeoplesProyect.run(debug=True, port=3300)
+
+if __name__ == "__main__":
+    thePeoplesProyect.run(port=3300)
