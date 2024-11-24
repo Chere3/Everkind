d6fdf5cb35dae61d35961cfeacce2cc1d23539ff
chere3 <71246795+Chere3@users.noreply.github.com>
2024-11-23 21:06:11 -0600

    refactor: database configuration for development environment

--- config.py
+++ config.py
@@ -5,16 +5,17 @@ class Config:
 
 
 class DevelopmentConfig(Config):
-    '''MYSQL_HOST = "localhost"
+    MYSQL_HOST = "localhost"
     MYSQL_USER = "root"
     MYSQL_PASSWORD = ""
-    MYSQL_DB = "thepeoplelproyect"'''
+    MYSQL_DB = "thepeoplelproyect"
 
-    # pythonanywhere
+    """# pythonanywhere
     MYSQL_HOST = "thepeoplesproyect.mysql.pythonanywhethepeopleproyectre-services.com"
     MYSQL_USER = "thepeoplesproyec"
     MYSQL_PASSWORD = "ucw8kgn6unt4znz@MVY"
     MYSQL_DB = "thepeoplesproyec$thepeoplesproyect"
+    """
 
 
 config = {"development": DevelopmentConfig}
